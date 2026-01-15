33558fde6a2e2a3069db0f96aad03783549a58d0
Thiago Medeiros <thiagotitico1@gmail.com>
2026-01-15 12:18:23 -0300

    Ativando o servidor para produção

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hellow world"
+    return "hello world"
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
